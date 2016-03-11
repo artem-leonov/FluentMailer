f8fc3f2d3317b251515305c87170057ad0ae6d0e
Леонов Артем <rash.scum@gmail.com>
2016-03-11 12:19:07 +0300

    shallow fixes

--- FluentMailer/src/FluentMailerMailSender.cs
+++ FluentMailer/src/FluentMailerMailSender.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Text.RegularExpressions;
-using System.Threading;
 using System.Threading.Tasks;
 using FluentMailer.Extensions;
 using FluentMailer.Interfaces;
